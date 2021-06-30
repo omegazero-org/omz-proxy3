6915f9fb5af04f34102c3085fac72ceac916bd49
user94729 <user94729@omegazero.org>
2021-06-30 17:12:37 +0200

    ArrayUtil: Added byteArrayIndexOf with start

--- src/org/omegazero/proxy/util/ArrayUtil.java
+++ src/org/omegazero/proxy/util/ArrayUtil.java
@@ -18,14 +18,30 @@ public final class ArrayUtil {
 
 
 	/**
-	 * Searches for the sequence of bytes given in the <b>seq</b> in the larger <b>arr</b> byte array.
+	 * Searches for the sequence of bytes given in the <b>seq</b> in the larger <b>arr</b> byte array.<br>
+	 * <br>
+	 * An invocation of this method of the form <code>byteArrayIndexOf(arr, seq)</code> is equivalent to
+	 * <code>{@link #byteArrayIndexOf(byte[], byte[], int) byteArrayIndexOf}(arr, seq, 0)</code>.
 	 * 
 	 * @param arr The array in which to search for <b>seq</b>
 	 * @param seq The sequence of bytes to search in the larger <b>arr</b> array
 	 * @return The index at which the given sequence starts in the <b>arr</b> array, or -1 if the sequence was not found
+	 * @see #byteArrayIndexOf(byte[], byte[], int)
 	 */
 	public static int byteArrayIndexOf(byte[] arr, byte[] seq) {
-		for(int i = 0; i < arr.length - seq.length + 1; i++){
+		return byteArrayIndexOf(arr, seq, 0);
+	}
+
+	/**
+	 * Searches for the sequence of bytes given in the <b>seq</b> in the larger <b>arr</b> byte array, starting at position <b>start</b>.
+	 * 
+	 * @param arr   The array in which to search for <b>seq</b>
+	 * @param seq   The sequence of bytes to search in the larger <b>arr</b> array
+	 * @param start The position to start searching for <b>seq</b>
+	 * @return The index at which the given sequence starts in the <b>arr</b> array, or -1 if the sequence was not found
+	 */
+	public static int byteArrayIndexOf(byte[] arr, byte[] seq, int start) {
+		for(int i = start; i < arr.length - seq.length + 1; i++){
 			boolean match = true;
 			for(int j = 0; j < seq.length; j++){
 				if(arr[i + j] != seq[j]){
